7c0b1b0df97fce5fd82348fa73c143e802db11ff
최민길 <cmg4739@gmail.com>
2020-05-01 04:03:50 +0900

    로그인, 회원가입 API

---
+++ controllers/MainController.php
@@ -0,0 +1,166 @@
+<?php
+require 'function.php';
+
+const JWT_SECRET_KEY = "TEST_KEYTEST_KEYTEST_KEYTEST_KEYTEST_KEYTEST_KEYTEST_KEYTEST_KEYTEST_KEYTEST_KEYTEST_KEYTEST_KEYTEST_KEY";
+
+$res = (Object)Array();
+header('Content-Type: json');
+$req = json_decode(file_get_contents("php://input"));
+try {
+    addAccessLogs($accessLogs, $req);
+    switch ($handler) {
+        /*
+         * API No. 0
+         * API Name : JWT 유효성 검사 테스트 API
+         * 마지막 수정 날짜 : 19.04.25
+         */
+        case "validateJwt":
+            // jwt 유효성 검사
+
+            $jwt = $_SERVER["HTTP_X_ACCESS_TOKEN"];
+
+            if (!isValidHeader($jwt, JWT_SECRET_KEY)) {
+                $res->isSuccess = FALSE;
+                $res->code = 201;
+                $res->message = "유효하지 않은 토큰입니다";
+                echo json_encode($res, JSON_NUMERIC_CHECK);
+                addErrorLogs($errorLogs, $res, $req);
+                return;
+            }
+
+            http_response_code(200);
+            $res->isSuccess = TRUE;
+            $res->code = 100;
+            $res->message = "테스트 성공";
+
+            echo json_encode($res, JSON_NUMERIC_CHECK);
+            break;
+
+        /*
+     * API No. 1
+     * API Name : 회원가입 API(재가입 가능)
+     * 마지막 수정 날짜 : 20.04.15
+     */
+        case "createUser":
+            http_response_code(200);
+
+            $email = $req->email;
+            $password = $req->password;
+            $phone = $req->phone;
+            $check1 = $req->is_over_14;
+            $check2 = $req->is_service_agree;
+            $check3 = $req->is_privacy_agree;
+            $check4 = $req->is_alarm_agree;
+
+            //값이 null값이 들어왔을 때 대처
+            if($email == null
+                or $password == null
+                or $phone == null){
+                $res->is_success = FALSE;
+                $res->code = 201;
+                $res->message = "빠진 항목을 다시 기입해주세요";
+                echo json_encode($res, JSON_NUMERIC_CHECK);
+                return;
+            }
+
+            //입력값 타입 검사
+            if(nl2br(gettype($email)) != "string"
+                or nl2br(gettype($password)) != "string"
+                or nl2br(gettype($phone)) != "string"){
+                $res->is_success = FALSE;
+                $res->code = 201;
+                $res->message = "유효한 타입이 아닙니다.";
+                echo json_encode($res, JSON_NUMERIC_CHECK);
+                return;
+            }
+
+            //동의 안하면 회원가입 못 함
+            if($check1 != "Y"
+            or $check2 != "Y"
+            or $check3 != "Y"){
+                $res->is_success = FALSE;
+                $res->code = 201;
+                $res->message = "필수 항목에 동의해주세요.";
+                echo json_encode($res, JSON_NUMERIC_CHECK);
+                return;
+            }
+
+            //잘못된 입력 거르기
+            if(!($check4 == "Y" or $check4 == "N")){
+                $res->is_success = FALSE;
+                $res->code = 201;
+                $res->message = "잘못된 입력입니다.";
+                echo json_encode($res, JSON_NUMERIC_CHECK);
+                return;
+            }
+
+            //이미 존재하는 회원인지 검토
+            if(isExistEmail($email)){
+
+                //삭제되었는지 확인 --> 삭제 시 재가입
+                if(isDeletedUser($email)){
+                    $res->result = recreateUser($email);
+                    $res->is_success = TRUE;
+                    $res->code = 100;
+                    $res->message = "회원가입을 축하드립니다!";
+                    echo json_encode($res, JSON_NUMERIC_CHECK);
+                    return;
+                }
+                else{
+                    $res->is_success = FALSE;
+                    $res->code = 201;
+                    $res->message = "이미 존재하는 회원입니다.";
+                    echo json_encode($res, JSON_NUMERIC_CHECK);
+                    return;
+                }
+            }
+
+            //중복되는 로그인 아이디인지 확인
+            if(isExistPhone($phone)){
+                $res->is_success = FALSE;
+                $res->code = 201;
+                $res->message = "이미 등록된 핸드폰 번호입니다.";
+                echo json_encode($res, JSON_NUMERIC_CHECK);
+                return;
+            }
+
+            $res->result = createUser($email, $password, $phone);
+            $res->isSuccess = TRUE;
+            $res->code = 100;
+            $res->message = "회원가입이 완료되었습니다.";
+            echo json_encode($res, JSON_NUMERIC_CHECK);
+            break;
+
+        /*
+         * API No. 2
+         * API Name : JWT 생성 테스트 API (로그인)
+         * 마지막 수정 날짜 : 19.04.25
+         */
+        case "createJwt":
+            // jwt 유효성 검사
+            http_response_code(200);
+
+            $email = $req->email;
+            $password = $req->password;
+
+            if(!isValidUser($email, $password)){
+                $res->is_success = FALSE;
+                $res->code = 100;
+                $res->message = "유효하지 않은 아이디 입니다";
+                echo json_encode($res, JSON_NUMERIC_CHECK);
+                return;
+            }
+
+            //페이로드에 맞게 다시 설정 요함
+            $jwt = getJWToken($email, $password, JWT_SECRET_KEY);
+            $res->result = $jwt;
+            $res->is_success = TRUE;
+            $res->code = 100;
+            $res->message = "테스트 성공";
+            echo json_encode($res, JSON_NUMERIC_CHECK);
+            break;
+
+    }
+} catch (\Exception $e) {
+    return getSQLErrorException($errorLogs, $e, $req);
+}
---
+++ pdos/IndexPdo.php
@@ -0,0 +1,206 @@
+<?php
+
+//회원가입 - 유저 추가 함수
+function createUser($email, $password, $phone){
+
+    $pdo = pdoSqlConnect();
+
+    $query = "INSERT INTO User (email, password, phone) VALUES (?,?,?);";
+    $st = $pdo->prepare($query);
+    //    $st->execute([$param,$param]);
+    $st->execute([$email, $password, $phone]);
+    $st->setFetchMode(PDO::FETCH_ASSOC);
+
+    $st=null;
+    $pdo = null;
+
+    return;
+}
+
+
+//-----------------------조건식--------------------------//
+
+//기존에 있는 이메일인지 판단
+function isExistEmail($email){
+
+    $pdo = pdoSqlConnect();
+    $query = "SELECT EXISTS(SELECT * FROM User where email = ?) as exist";
+
+
+    $st = $pdo->prepare($query);
+    //    $st->execute([$param,$param]);
+    $st->execute([$email]);
+    $st->setFetchMode(PDO::FETCH_ASSOC);
+    $res = $st->fetchAll();
+
+    $st=null;$pdo = null;
+
+    return intval($res[0]["exist"]);
+}
+
+//기존에 있는 번호인지 판단
+function isExistPhone($phone){
+
+    $pdo = pdoSqlConnect();
+    $query = "SELECT EXISTS(SELECT * FROM User where phone = ?) as exist";
+
+
+    $st = $pdo->prepare($query);
+    //    $st->execute([$param,$param]);
+    $st->execute([$phone]);
+    $st->setFetchMode(PDO::FETCH_ASSOC);
+    $res = $st->fetchAll();
+
+    $st=null;$pdo = null;
+
+    return intval($res[0]["exist"]);
+}
+
+//삭제된 유저인지 판단
+function isDeletedUser($email){
+
+    $pdo = pdoSqlConnect();
+    $query = "SELECT EXISTS(SELECT * FROM User where email = ? and isDeleted = 'Y') as exist";
+
+
+    $st = $pdo->prepare($query);
+    //    $st->execute([$param,$param]);
+    $st->execute([$email]);
+    $st->setFetchMode(PDO::FETCH_ASSOC);
+    $res = $st->fetchAll();
+
+    $st=null;$pdo = null;
+
+    return intval($res[0]["exist"]);
+}
+
+function isValidUser($email, $password){
+    $pdo = pdoSqlConnect();
+    $query = "SELECT EXISTS(SELECT * FROM User WHERE email= ? AND password = ?) AS exist;";
+
+
+    $st = $pdo->prepare($query);
+    //    $st->execute([$param,$param]);
+    $st->execute([$email, $password]);
+    $st->setFetchMode(PDO::FETCH_ASSOC);
+    $res = $st->fetchAll();
+
+    $st=null;$pdo = null;
+
+    return intval($res[0]["exist"]);
+
+}
+
+//삭제 유저 재가입 함수
+function recreateUser($email){
+
+    $pdo = pdoSqlConnect();
+
+    $query = "UPDATE User SET isDeleted = 'N' where email = ?;";
+    $st = $pdo->prepare($query);
+    //    $st->execute([$param,$param]);
+    $st->execute([$email]);
+    $st->setFetchMode(PDO::FETCH_ASSOC);
+
+    $st=null;
+    $pdo = null;
+
+    return;
+}
+
+
+// CREATE
+//    function addMaintenance($message){
+//        $pdo = pdoSqlConnect();
+//        $query = "INSERT INTO MAINTENANCE (MESSAGE) VALUES (?);";
+//
+//        $st = $pdo->prepare($query);
+//        $st->execute([$message]);
+//
+//        $st = null;
+//        $pdo = null;
+//
+//    }
+
+
+// UPDATE
+//    function updateMaintenanceStatus($message, $status, $no){
+//        $pdo = pdoSqlConnect();
+//        $query = "UPDATE MAINTENANCE
+//                        SET MESSAGE = ?,
+//                            STATUS  = ?
+//                        WHERE NO = ?";
+//
+//        $st = $pdo->prepare($query);
+//        $st->execute([$message, $status, $no]);
+//        $st = null;
+//        $pdo = null;
+//    }
+
+// RETURN BOOLEAN
+//    function isRedundantEmail($email){
+//        $pdo = pdoSqlConnect();
+//        $query = "SELECT EXISTS(SELECT * FROM USER_TB WHERE EMAIL= ?) AS exist;";
+//
+//
+//        $st = $pdo->prepare($query);
+//        //    $st->execute([$param,$param]);
+//        $st->execute([$email]);
+//        $st->setFetchMode(PDO::FETCH_ASSOC);
+//        $res = $st->fetchAll();
+//
+//        $st=null;$pdo = null;
+//
+//        return intval($res[0]["exist"]);
+//
+//    }
+
+//READ
+function test()
+{
+    $pdo = pdoSqlConnect();
+    $query = "SELECT * FROM Test;";
+
+    $st = $pdo->prepare($query);
+    //    $st->execute([$param,$param]);
+    $st->execute();
+    $st->setFetchMode(PDO::FETCH_ASSOC);
+    $res = $st->fetchAll();
+
+    $st = null;
+    $pdo = null;
+
+    return $res;
+}
+
+//READ
+function testDetail($testNo)
+{
+    $pdo = pdoSqlConnect();
+    $query = "SELECT * FROM Test WHERE no = ?;";
+
+    $st = $pdo->prepare($query);
+    $st->execute([$testNo]);
+    //    $st->execute();
+    $st->setFetchMode(PDO::FETCH_ASSOC);
+    $res = $st->fetchAll();
+
+    $st = null;
+    $pdo = null;
+
+    return $res[0];
+}
+
+
+function testPost($name)
+{
+    $pdo = pdoSqlConnect();
+    $query = "INSERT INTO Test (name) VALUES (?);";
+
+    $st = $pdo->prepare($query);
+    $st->execute([$name]);
+
+    $st = null;
+    $pdo = null;
+
+}
